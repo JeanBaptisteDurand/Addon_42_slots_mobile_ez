4eb6e3fa83fef4439d0c5431eed2675d89370b35
Jean-Baptiste Durand <jbdurand2000@gmail.com>
2025-11-08 16:30:56 +0100

    fix create addon script to not zip the folder but only the content

--- create_addon.sh
+++ create_addon.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 VERSION=$(grep -oP '"version"\s*:\s*"\K[^"]+' 42_slots/manifest.json)
-zip -r "42-slots-$VERSION.xpi" 42_slots -x "42_slots/.git/*" "42_slots/node_modules/*"
+cd 42_slots || exit
+zip -r "../42-slots-$VERSION.xpi" . -x ".git/*" "node_modules/*"
